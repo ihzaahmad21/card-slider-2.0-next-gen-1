a68aea0f8147439a2450069a3874c50b43c29cc7
ihzaahmad21 <jogal102939@gmail.com>
2026-08-07 01:41:15 +0700

    web

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-});
+  base: '/card-slider-2.0-next-gen-1/', // 👈 Wajib ada nama repo ini
+})
